859df7167ae923dea1b88d269d07e04d60ff931d
Yichao Wu <yichao.prime@osaka-u.ac.jp>
2025-04-01 01:12:34 +0900

    correct function.js

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
 function showMessage() {
     value = document.getElementById("input").value;
-    document.getElementById("textresult").value = value;
+    document.getElementById("output").value = value;
 }
